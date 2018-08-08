925ccf7ea1045b787350e453777d93a269bb0059
Alex Varjabedian <alex@trueandco.com>
2018-08-08 14:23:06 -0700

    got vuejs to work with rails

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -5,18 +5,26 @@
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
-import Vue from 'vue'
-import App from '../app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('hello'))
-  const app = new Vue({
-    el,
-    render: h => h(App)
-  })
+// Commenting the following
 
-  console.log(app)
-})
+// import Vue from 'vue'
+// import App from '../app.vue'
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const el = document.body.appendChild(document.createElement('hello'))
+//   const app = new Vue({
+//     el,
+//     render: h => h(App)
+//   })
+
+//   console.log(app)
+// })
+
+
+
+
+
 
 
 // The above code uses Vue without the compiler, which means you cannot
@@ -33,21 +41,25 @@ document.addEventListener('DOMContentLoaded', () => {
 // </div>
 
 
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
-//
-//
-//
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    el: '#app',
+    data: {
+      message: "Can you say hello?"
+    },
+    components: { App }
+  })
+})
+
+
+
+
+
+
+
 // If the using turbolinks, install 'vue-turbolinks':
 //
 // yarn add 'vue-turbolinks'
